f0dbe428c8af79fa8359c398f7a0ca3e3c5e9e9d
Veries Seals <veries@me.com>
2025-03-03 18:13:50 -0600

    Ternary Operator Lesson

--- src/Hello.java
+++ src/Hello.java
@@ -48,6 +48,22 @@ public class Hello {
         
         // Lesson 39 Ternary Operator
         // --------------------------------------------
+        
+        String makeOfCar = "Volkswagen";
+        boolean isDomestic = makeOfCar == "Volkswagen" ? false : true;
+        
+        if (isDomestic) {
+            System.out.println("This car is domestic to our country");
+        }
+        
+        String s = (isDomestic) ? "This car is domestic" : "This car is not domestic";
+        
+        System.out.println(s);
+        
+        // Lesson 40 Operator Presedence 
+        // --------------------------------------------
+
+        
 
 
     }
