c87eb39101a14184bb9690447e993fba7ab690f9
Veries Seals <veries@me.com>
2025-03-03 19:38:14 -0600

    Lesson 38 Challenge

--- src/Hello.java
+++ src/Hello.java
@@ -60,10 +60,34 @@ public class Hello {
         
         System.out.println(s);
         
+        // Lesson Challenge
+        // --------------------------------------------
+        // Step 1:  create a double variable with a value of 20.00.
+        double myFirstVariable = 20.00;
+
+        // Step 2:  create a second variable of type double with a value 80.00.
+        double mySecondVariable = 80.00;
+
+        // Step 3:  add both numbers together, then multiply by 100.00.
+        double myTotalValue = ((myFirstVariable + mySecondVariable) * 100);
+        System.out.println("myTotalValue * 100 is " + myTotalValue);
+
+        // Step 4:  use the remainder operator, to figure out what the remainder from the result of the operation in step three, and 40.00, will be.
+
+        double myRemainder = myTotalValue % 2;
+        System.out.println("myRemainder is " + myRemainder);
+
+        // Step 5:  create a boolean variable that assigns the value true, if the remainder in step four is 0.00, or false if it's not zero.
+
+        // Step 6:  output the boolean variable just to see what the result is. 
+
+        // Step 7:  write an if-then statement that displays a message, 'got some remainder', if the boolean in step five is not true.
+
+
         // Lesson 40 Operator Presedence 
         // --------------------------------------------
 
-        
+
 
 
     }
